82104c6ade52090c03ed5d900dcae1fdc492b0b5
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-09-10 20:55:42 -0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -180,12 +180,10 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award-winning development studio based in Denmark.
+            Zerofour is a global private security and intelligence agency. We assist governments, corporations, and high-profile individuals.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            We are a development studio working at the intersection of design
-            and technology. It’s a really busy intersection though — a lot of
-            our staff have been involved in hit and runs.
+            At Zerofour, our number one priority is to secure success for our clients. Zerofour goes where government resources simply can't or won't. We protect critical facilities, assets, and personnel effectively and efficiently around the globe. Our team brings unparalleled dedication and passion for creating a safer world and upholding the highest standards of compliance, quality, and integrity. Zerofour is a leading global provider of private security, logistics operations, risk management, and humanitarian, training, and operational support services to government and commercial clients worldwide.
           </p>
         </FadeIn>
       </Container>
