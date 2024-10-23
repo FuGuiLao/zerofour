cda0ac1f5b5ca971337386c579f30a0d853fcfba
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-10-23 18:58:43 -0400

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ export function Logomark({ invert = false, filled = false, ...props }) {
   let id = useId()
 
   return (
-    <svg viewBox="0 0 471.73 517.77" aria-hidden="true" {...props}>
+    <svg width="100" height="100" viewBox="0 0 471.73 517.77" aria-hidden="true" {...props}>
       <rect
         clipPath={`url(#${id}-clip)`}
         className={clsx(
