39c52c9a6dbf70cbe3393bd77665b9fd7a2e7722
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-10-23 21:26:53 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -124,7 +124,7 @@ export function Footer() {
             <Logo className="h-8" fillOnHover />
           </Link>
           <p className="text-sm text-neutral-700">
-            Copyright © {new Date().getFullYear()} Zerofour Inc. All Rights Reserved.   
+            © {new Date().getFullYear()} Zerofour Inc. All Rights Reserved.   
           </p>
         </div>
       </FadeIn>
