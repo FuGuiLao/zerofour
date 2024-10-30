7eb4b1dced89cb01fc8067658c958643a229a8c6
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-10-29 23:54:31 -0700

    Update RootLayout.jsx

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -60,12 +60,12 @@ function Header({
           onMouseLeave={() => setLogoHovered(false)}
         >
           <Logomark
-            className="h-8 sm:hidden"
+            className="h-12 sm:hidden"
             invert={invert}
             filled={logoHovered}
           />
           <Logo
-            className="hidden h-20 sm:block"
+            className="hidden h-32 sm:block"
             invert={invert}
             filled={logoHovered}
           />
