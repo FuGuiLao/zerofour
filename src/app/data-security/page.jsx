5dfc800963c71672896016bc9a9ee56111f6880c
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-10-31 20:52:20 -0700

    Update page.jsx

--- src/app/data-security/page.jsx
+++ src/app/data-security/page.jsx
@@ -198,7 +198,7 @@ compliant and utilize the strongest industry recognized security, including TLS 
         <p>All client data is redundantly stored multiple times in multiple physical locations to ensure the highest level of data
 integrity and the entire network is housed in SAS70 Type II data centers.</p>
 <p>To give notice, or if you have any questions or comments about data security, please contact us in writing.</p>
-          <p>&nbsp;</p>
+      <br />
 Zerofour Inc.<br />
 515 S. Flower Street 18th Floor<br />
 Los Angeles, CA 90071
