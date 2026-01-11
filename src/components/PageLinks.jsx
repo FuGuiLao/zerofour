9c479eba4927f586288f5315a2265b938a9062b2
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2026-01-11 14:23:51 -0800

    Update PageLinks.jsx

--- src/components/PageLinks.jsx
+++ src/components/PageLinks.jsx
@@ -42,7 +42,7 @@ function PageLink({ page }) {
           className="mt-6 flex gap-x-3 text-base font-semibold text-neutral-950 transition hover:text-neutral-700"
           aria-label={`Read more: ${page.title}`}
         >
-          Read more
+          Read More
           <ArrowIcon className="w-6 flex-none fill-current" />
           <span className="absolute inset-0" />
         </Link>
