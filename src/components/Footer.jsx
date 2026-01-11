3ad9ba37763ce8199c296ea9d84837cc1795e700
FuGuiLao <EAGLEMINY@GMAIL.COM>
2026-01-11 01:47:51 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ import { Container } from '@/components/Container'
 import { FadeIn } from '@/components/FadeIn'
 import { Logo } from '@/components/Logo'
 import { socialMediaProfiles } from '@/components/SocialMedia'
+import { Button } from '@/components/Button'
+
 
 const navigation = [
   {
