b9611d87facf3108ee9c5872c7059dd1e6f596f0
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2026-01-12 01:23:53 -0800

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -57,15 +57,15 @@ const team = [
     title: 'Leadership',
     people: [
       {
-        name: 'Leslie Alexander',
-        role: 'Co-Founder / CEO',
+        name: 'Jayden Brant',
+        role: 'Director / Operations',
         image: { src: imageLeslieAlexander },
         blurb:
-          "Our network of special operators from the world's elite military units, combined with seasoned intelligence professionals, allows Zerofour to operate at the highest level to serve our clients.",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       },
       {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
+        name: 'Brett Montgomery',
+        role: 'Director / Administration',
         image: { src: imageMichaelFoster },
         blurb:
           "Zerofour leverages advanced technology, systems integration, and intelligence-driven platforms to protect critical infrastructure, optimize asset visibility, and enable decisive action in complex environments.",
