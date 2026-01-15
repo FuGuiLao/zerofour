b8cf1d53a4cd4329fd736c6d156ebc94edea29e8
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2026-01-14 17:16:45 -0800

    Update TextArea.jsx

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -6,8 +6,8 @@ export function TextArea({ label, rows = 6, className, ...props }) {
       rows={rows}
       placeholder={label}
       className={clsx(
-        // MATCH TextInput row styling
-        'block w-full resize-y border-t border-neutral-200 bg-transparent px-6 py-6 text-base text-neutral-950 placeholder:text-neutral-500 focus:outline-none focus:ring-0',
+        // Matches TextInput row border + padding
+        'relative block w-full resize-y rounded-none border border-neutral-200 bg-transparent px-6 py-6 text-base text-neutral-950 placeholder:text-neutral-500 focus:z-10 focus:border-neutral-950 focus:outline-none focus:ring-0',
         className
       )}
       {...props}
