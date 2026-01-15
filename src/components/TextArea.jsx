634c69c64fd41512a45ca6e2216d8729b0bd0b5f
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2026-01-14 17:21:59 -0800

    Update TextArea.jsx

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -7,7 +7,7 @@ export function TextArea({ label, rows = 6, className, ...props }) {
       placeholder={label}
       className={clsx(
         // Match TextInput exactly
-        'relative block w-full resize-y border border-neutral-200 bg-transparent px-6 py-6 text-base text-neutral-950 placeholder:text-neutral-500 focus:z-10 focus:border-neutral-950 focus:outline-none focus:ring-0',
+        'relative block w-full resize-y border border-neutral-300 bg-transparent px-6 py-6 text-base text-neutral-950 placeholder:text-neutral-500 focus:z-10 focus:border-neutral-950 focus:outline-none focus:ring-0',
 
         // 👇 THIS is the missing piece
         'last:rounded-b-2xl',
