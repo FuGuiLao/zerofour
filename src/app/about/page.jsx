8ec54a220f7bc77366baa34fc722539406670c35
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2026-02-26 21:45:30 -0800

    Enhance image with hover transition effect
    Added transition effect to image on hover.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -171,7 +171,7 @@ function Team() {
                       <div className="lg:flex lg:gap-12">
                         <div className="lg:w-[22rem] lg:flex-none">
                           <div className="relative overflow-hidden rounded-3xl">
-  <Image alt="" {...person.image} className="h-96 w-full object-cover" />
+  <Image alt="" {...person.image} className="h-96 w-full object-cover transition duration-500 motion-safe:group-hover:scale-105" />
   <div className="absolute inset-0 bg-gradient-to-t from-black to-black/0 to-40%" />
   <div className="absolute inset-0 flex flex-col justify-end p-6">
     <p className="font-display text-base/6 font-semibold tracking-wide text-white">
