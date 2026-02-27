213ec82813ccc01047266806eed016e493636397
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2026-02-26 18:31:10 -0800

    Update gradient opacity in about-test page

--- src/app/about-test/page.jsx
+++ src/app/about-test/page.jsx
@@ -223,7 +223,7 @@ function Team() {
                             {...person.image}
                             className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
                           />
-                          <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
+                          <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-100% p-6">
                             <p className="font-display text-base/6 font-semibold tracking-wide text-white">
                               {person.name}
                             </p>
